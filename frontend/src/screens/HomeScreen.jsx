15f16290a366787358bc960f2cb30ed225ae1329
ssr0016 <samsonrecluta0016@gmail.com>
2025-09-29 12:50:53 +0800

    serving & fetching data, starting mongoDB & mongoose, Redux toolkit setup & state management

---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,33 @@
+import { Row, Col } from "react-bootstrap";
+import Products from "../components/Products";
+import { useGetProductsQuery } from "../slices/productsApiSlice.js";
+import Loader from "../components/Loader.jsx";
+import Message from "../components/Message.jsx";
+const HomeScreen = () => {
+  const { data: products, isLoading, error } = useGetProductsQuery();
+
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
+        <>
+          <h1>Latest Product</h1>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Products product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
+    </>
+  );
+};
+
+export default HomeScreen;
