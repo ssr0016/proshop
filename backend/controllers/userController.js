acb82660dfe9703db980d3c032a48c0385a138ed
ssr0016 <samsonrecluta0016@gmail.com>
2025-09-30 18:37:08 +0800

    logout user & clear cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -46,7 +46,12 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route POST /api/users/logout
 // @access Private
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send("logout User");
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: "Successfully logged out" });
 });
 
 // @desc Get User Profile
