3a5c4d4b575f4afc39d9947a950d7e3cec313dcd
ssr0016 <samsonrecluta0016@gmail.com>
2025-10-01 11:18:11 +0800

    login functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Container } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         </Container>
       </main>
       <Footer />
+      <ToastContainer />
     </>
   );
 };
