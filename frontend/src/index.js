7cfcfc1173aba6ba1d87d82b82e29b2a0d9a9ead
ssr0016 <samsonrecluta0016@gmail.com>
2025-10-02 13:05:59 +0800

    payment method

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -20,6 +20,7 @@ import LoginScreen from "./screens/LoginScreen.jsx";
 import RegisterScreen from "./screens/RegisterScreen.jsx";
 import ShippingScreen from "./screens/ShippingScreen.jsx";
 import PrivateRoute from "./components/PrivateRoute.jsx";
+import PaymentScreen from "./screens/PaymentScreen.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -32,6 +33,7 @@ const router = createBrowserRouter(
 
       <Route path="" element={<PrivateRoute />} />
       <Route index={true} path="/shipping" element={<ShippingScreen />} />
+      <Route index={true} path="/payment" element={<PaymentScreen />} />
     </Route>
   )
 );
